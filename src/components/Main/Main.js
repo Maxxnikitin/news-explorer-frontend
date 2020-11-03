8fc60f537743ad93b93aab548f9ae1aedf5e2cc9
Max Nikitin <maxxnikitin90@yandex.ru>
2020-11-04 00:57:42 +0300

    add CardListButton component and update NewsCardList

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css';
 import Preloader from '../Preloader/Preloader';
 import NotFoundCard from '../NotFoundCard/NotFoundCard';
+import NewsCardList from '../NewsCardList/NewsCardList';
 import About from '../About/About';
 
 function Main() {
   return (
     <main className='main'>
       <Preloader />
+      <NewsCardList title='Результаты поиска' />
       <NotFoundCard />
       <About />
     </main>
