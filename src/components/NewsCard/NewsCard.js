b5c00845b511b9c038e58fb78c138e58de39e440
Maxx Nikitin <maxxnikitin90@yandex.ru>
2020-11-18 10:17:34 +0300

    add image link to articles

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,72 +1,115 @@
-import React from 'react';
-import './NewsCard.css';
-import CardButton from '../ui/CardButton/CardButton';
-import iconAdd from '../../images/news-card/add-not-active.svg'
-import iconAddHover from '../../images/news-card/add-hover.svg'
-import iconAddMarked from '../../images/news-card/add-marked.svg'
-import iconDel from '../../images/news-card/del-not-active.svg'
-import iconDelHover from '../../images/news-card/del-hover.svg'
-import { api } from '../../utils/MainApi';
+import React from "react";
+import "./NewsCard.css";
+import CardButton from "../ui/CardButton/CardButton";
+import iconAdd from "../../images/news-card/add-not-active.svg";
+import iconAddHover from "../../images/news-card/add-hover.svg";
+import iconAddMarked from "../../images/news-card/add-marked.svg";
+import iconDel from "../../images/news-card/del-not-active.svg";
+import iconDelHover from "../../images/news-card/del-hover.svg";
+import { api } from "../../utils/MainApi";
 
 function NewsCard({
-  id, title, text, source, image, date, link, setSavedArticles, page, src, tooltip, keyword
+  id,
+  title,
+  text,
+  source,
+  image,
+  date,
+  link,
+  setSavedArticles,
+  page,
+  src,
+  tooltip,
+  keyword,
 }) {
   const [cardButtonMouseEnter, setCardButtonMouseEmter] = React.useState(false);
-  const [cardButtonImg, setCardButtonImg] = React.useState((page === 'main') ? iconAdd : iconDel);
+  const [cardButtonImg, setCardButtonImg] = React.useState(
+    page === "main" ? iconAdd : iconDel
+  );
   const [isSave, setIsSave] = React.useState(false);
 
-  const tooltipClassList = `${cardButtonMouseEnter ? 'card__tooltip card__tooltip_active' : 'card__tooltip '}`
+  const tooltipClassList = `${
+    cardButtonMouseEnter
+      ? "card__tooltip card__tooltip_active"
+      : "card__tooltip "
+  }`;
 
   function handleCardButtonMouseEnter() {
     setCardButtonMouseEmter(true);
-    page !== 'main' ? setCardButtonImg(iconDelHover) : cardButtonImg === iconAddMarked ? setCardButtonImg(iconAddMarked) : setCardButtonImg(iconAddHover);
+    page !== "main"
+      ? setCardButtonImg(iconDelHover)
+      : cardButtonImg === iconAddMarked
+      ? setCardButtonImg(iconAddMarked)
+      : setCardButtonImg(iconAddHover);
   }
 
   function handleCardButtonMouseLeave() {
     setCardButtonMouseEmter(false);
-    page !== 'main' ? setCardButtonImg(iconDel) : isSave ? setCardButtonImg(iconAddMarked) : setCardButtonImg(iconAdd);
+    page !== "main"
+      ? setCardButtonImg(iconDel)
+      : isSave
+      ? setCardButtonImg(iconAddMarked)
+      : setCardButtonImg(iconAdd);
   }
 
   function handleDelete() {
     setCardButtonImg(iconDelHover);
-    api.deleteArticle(id)
+    api
+      .deleteArticle(id)
       .then((res) => {
         if (res.ok) {
-          api.getAllArticles()
+          api.getAllArticles();
         }
       })
       .then((data) => setSavedArticles(data))
       .catch((err) => console.error(err));
-  };
+  }
 
   function handleSave() {
-    setCardButtonImg(iconAddMarked)
-    api.saveArticle(keyword, title, text, date, source, link, image)
+    setCardButtonImg(iconAddMarked);
+    console.log({
+      keyword: keyword,
+      title: title,
+      text: text,
+      source: source,
+      date: date,
+      link: link,
+      image: image,
+    });
+    api
+      .saveArticle(keyword, title, text, date, source, link, image)
       .then(() => setIsSave(!isSave))
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   }
 
   function handleCardButtonClick() {
-    page !== 'main' ?
-    handleDelete() : handleSave()
+    page !== "main" ? handleDelete() : handleSave();
   }
 
   return (
-    <article className='card'>
-      <a className='card__link' href={link} target='_blank' rel='noreferrer'>
-        <img className='card__img' src={src} alt='изображение новостной статьи.' />
-        <div className='card__container'>
-          <p className='card__date'>{date}</p>
-          <h3 className='card__title'>{title}</h3>
-          <p className='card__text'>{text}</p>
-          <p className='card__source'>{source}</p>
+    <article className="card">
+      <a className="card__link" href={link} target="_blank" rel="noreferrer">
+        <img
+          className="card__img"
+          src={src}
+          alt="изображение новостной статьи."
+        />
+        <div className="card__container">
+          <p className="card__date">{date}</p>
+          <h3 className="card__title">{title}</h3>
+          <p className="card__text">{text}</p>
+          <p className="card__source">{source}</p>
         </div>
       </a>
-      <CardButton src={cardButtonImg} alt='добавить в избранное.' onMouseEnter={handleCardButtonMouseEnter} onMouseLeave={handleCardButtonMouseLeave} onClick={handleCardButtonClick} />
+      <CardButton
+        src={cardButtonImg}
+        alt="добавить в избранное."
+        onMouseEnter={handleCardButtonMouseEnter}
+        onMouseLeave={handleCardButtonMouseLeave}
+        onClick={handleCardButtonClick}
+      />
       <div className={tooltipClassList}>{tooltip}</div>
-      {(page !== 'main') ?
-      <div className='card__teg-name'>{keyword}</div>
-      : ''}
+      {page !== "main" ? <div className="card__teg-name">{keyword}</div> : ""}
     </article>
   );
 }
--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,77 +1,89 @@
-import React from 'react';
-import './NewsCardList.css';
-import NewsCard from '../NewsCard/NewsCard';
-import CardListButton from '../ui/CardListButton/CardListButton';
-import { api } from '../../utils/MainApi';
-import { toArticleDate } from '../../utils/Date';
+import React from "react";
+import "./NewsCardList.css";
+import NewsCard from "../NewsCard/NewsCard";
+import CardListButton from "../ui/CardListButton/CardListButton";
+import { api } from "../../utils/MainApi";
+import { toArticleDate } from "../../utils/Date";
 
 function NewsCardList(props) {
   const [countArticle, setCountArticle] = React.useState(3);
 
   function handleAddArticles() {
     setCountArticle(countArticle + 3);
-  };
+  }
 
   function getSavedArticles() {
     if (props.loggedIn) {
-      api.getAllArticles()
+      api
+        .getAllArticles()
         .then((res) => {
           props.setSavedArticles(res);
         })
         .catch((err) => console.error(err));
     }
-  };
+  }
 
   React.useEffect(() => {
     getSavedArticles();
   }, []);
 
   return (
-    <section className='news-card-list'>
-      {(props.page === 'main') ? <h2 className='news-card-list__title'>Результаты поиска</h2> : ''}
-      <div className='news-card-list__container'>
-        {(props.page !== 'main') ?
-        <>
-          {
-            props.savedArticles.map(item => {
-              return (<NewsCard
-                title={item.title}
-                text={item.text}
-                date={item.date}
-                author={item.name}
-                link={item.url}
-                key={Math.random() * 10000000}
-                tooltip={props.tooltip}
-                id={item._id}
-                src={item.image}
-                isSaved={true}
-                tag={item.keyword}
-                page={props.page}
-                setSavedArticles={props.setSavedArticles}
-              />);
-            })
-          }
-        </> :
-        <>
-          {
-            props.articles.slice(0, countArticle).map(item => {
-              return (<NewsCard
-                src={item.urlToImage}
-                link={item.url}
-                title={item.title}
-                text={item.description}
-                source={item.source.name}
-                key={Math.random() * 10000000}
-                date={toArticleDate}
-                page={props.page}
-                keyword={props.keyword}
-                tooltip={props.tooltip}
-              />);
+    <section className="news-card-list">
+      {props.page === "main" ? (
+        <h2 className="news-card-list__title">Результаты поиска</h2>
+      ) : (
+        ""
+      )}
+      <div className="news-card-list__container">
+        {props.page !== "main" ? (
+          <>
+            {props.savedArticles.map((item) => {
+              return (
+                <NewsCard
+                  title={item.title}
+                  text={item.text}
+                  date={item.date}
+                  author={item.name}
+                  image={item.image}
+                  link={item.url}
+                  key={Math.random() * 10000000}
+                  tooltip={props.tooltip}
+                  id={item._id}
+                  isSaved={true}
+                  tag={item.keyword}
+                  page={props.page}
+                  setSavedArticles={props.setSavedArticles}
+                />
+              );
             })}
           </>
-        }
+        ) : (
+          <>
+            {props.articles.slice(0, countArticle).map((item) => {
+              return (
+                <NewsCard
+                  src={item.urlToImage}
+                  link={item.url}
+                  title={item.title}
+                  text={item.description}
+                  source={item.source.name}
+                  key={Math.random() * 10000000}
+                  date={toArticleDate}
+                  page={props.page}
+                  image={item.urlToImage}
+                  keyword={props.keyword}
+                  tooltip={props.tooltip}
+                />
+              );
+            })}
+          </>
+        )}
       </div>
-      {((props.page === 'main') && (countArticle < props.articles.length)) ? <CardListButton text='Показать ещё' onClick={handleAddArticles} /> : ''}
+      {props.page === "main" && countArticle < props.articles.length ? (
+        <CardListButton text="Показать ещё" onClick={handleAddArticles} />
+      ) : (
+        ""
+      )}
     </section>
   );
 }
